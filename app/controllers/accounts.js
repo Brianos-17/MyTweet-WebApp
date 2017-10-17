ed375edfb13b0ab1d45838caf54caae725725ab9
Brian <brianos-17git@hotmail.com>
2017-10-17 14:47:25 +0100

    Wrote register method for new users. Included supporting route

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -18,6 +18,19 @@ exports.signup = {
   },
 };
 
+exports.register = {
+  auth: false,
+  handler: function(req, res) {
+    const user = new User(req.payload);
+    user.save().then(newUser => {
+      res.redirect('/login');
+    }).catch(err => {
+      res.redirect('/');
+      console.log('Error registering new user: ' + err);
+    });
+  },
+};
+
 exports.login = {
   auth: false,
   handler: function(req, res) {
--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = [
   { method: 'GET',    path: '/',       config: Accounts.main },
   { method: 'GET',    path: '/signup', config: Accounts.signup },
   { method: 'GET',    path: '/login',  config: Accounts.login},
+
+  { method: 'POST',   path: '/register', config: Accounts.register},
 ];

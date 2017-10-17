2e5b5e5b97da73cf5e4e33de78981194f4e8e43c
Brian <brianos-17git@hotmail.com>
2017-10-17 15:19:58 +0100

    Fixed bug in authenticate method

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -25,6 +25,8 @@ exports.register = {
   handler: function(req, res) {
     const user = new User(req.payload);
     user.save().then(newUser => {
+      console.log('New user registered: ' + newUser.firstName +
+          ' ' + newUser.lastName);
       res.redirect('/login');
     }).catch(err => {
       res.redirect('/');
@@ -38,6 +40,27 @@ exports.login = {
   handler: function(req, res) {
     res.view('login', {
       title: "Log In to MyTweet"
+    });
+  },
+};
+
+exports.authenticate = {
+  auth: false,
+  handler: function (req, res) {
+    const user = req.payload;
+    User.findOne({email: user.email}).then(foundUser => {
+      if (foundUser && foundUser.password === user.password) {
+        req.cookieAuth.set({
+          loggedIn: true,
+        loggedInUser: user.email,
+        });
+        res.redirect('/home');
+      } else {
+        res.redirect('/login');
+      }
+    }).catch(err => {
+      console.log('Error logging in: ' + err);
+      res.redirect('/');
     });
   },
 };
--- routes.js
+++ routes.js
@@ -5,5 +5,6 @@ module.exports = [
   { method: 'GET',    path: '/signup', config: Accounts.signup },
   { method: 'GET',    path: '/login',  config: Accounts.login},
 
-  { method: 'POST',   path: '/register', config: Accounts.register},
+  { method: 'POST',   path: '/register',      config: Accounts.register},
+  { method: 'POST',   path: '/authenticate',  config: Accounts.authenticate},
 ];
